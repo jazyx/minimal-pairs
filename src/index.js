57c8f268356dc7042c42371889fd7f0919b6c016
James Newton <blackslate@lexogram.com>
2021-09-15 12:47:38 +0300

    Move Tap Here and (>) button to top of pocket
    * This makes the app usable even when the app is not scrolled up on mobile
    * Background colour purple

--- src/index.js
+++ src/index.js
@@ -16,6 +16,13 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
+window.addEventListener("load",function() {
+  setTimeout(function(){
+      // This hides the address bar:
+      window.scrollTo(0, 1);
+  }, 0);
+});
+
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
