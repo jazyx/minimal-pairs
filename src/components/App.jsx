56cae75ea5df9f146a7eba3e07bb8118e407f3c7
James Newton <blackslate@lexogram.com>
2021-11-08 00:20:49 +0300

    Add left-handed checkbox to Preferences
    * App.jsx
      + Create mainRef and mainElement (using useRef and useEffect)
      + Add classNameIsSet() and toggleClassName() functions
      + Pass these functions to all Views (especially Prefeneces)
    * Prefences.jsx
      + Ask App.jsx whether "left-handed" class is classNameIsSet
      + Show checkbox
    
    TODO:
    + Save left-handed state to localStorage, and read it in on startUp
    + Use slider control for left-handed checkbox

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,17 @@
-import React, { useState } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import { AudioProvider } from "./AudioContext"
 
 import Views from "../api/views";
 import Menu from "./Menu";
 
 
+let mainElement
+
+
 const App = (props) => {
   const [ view, setView ] = useState("About")
+  const mainRef = useRef()
+
 
   const selectFromMenu = (newView) => {
     if (typeof newView !== "string") {
@@ -16,13 +21,48 @@ const App = (props) => {
     setView(newView)
   }
 
+
+  const classNameIsSet = (className) => {
+    if (!mainElement) {
+      console.log("Can't get className until mainElement is initialized")
+      return
+    }
+
+    return mainElement.classList.contains(className)
+  }
+
+
+  const toggleClassName = (className, on) => {
+    if (!mainElement) {
+      console.log("Can't set className until mainElement is initialized")
+      return
+    }
+
+    if (on === false) {
+      mainElement.classList.remove(className)
+    } else {
+      mainElement.classList.add(className)
+    }
+  }
+
+
+  useEffect(() => {
+    mainElement = mainRef.current
+  }, [])
+
+
   const View = Views[view];
 
   return (
     <AudioProvider>
-      <main className="split left-handed show--phonetic show-cue-image">
+      <main
+        className="split left--handed show--phonetic show-cue-image"
+        ref={mainRef}
+      >
         <View
           startActivity={selectFromMenu}
+          toggleClassName={toggleClassName}
+          classNameIsSet={classNameIsSet}
         />
         <Menu
           selectFromMenu={selectFromMenu}
--- src/components/Preferences.jsx
+++ src/components/Preferences.jsx
@@ -12,15 +12,40 @@
  *   Left | Right-handed
  *   Split layout when the screen is wide enough
  * Show|Hide cue image
+ * Interface language
+ * Target language
  */
 
 
-import React from 'react';
+import React, { useState } from 'react';
 
 
-const Preferences = (props) => {
- return (
-   <h1>Preferences page goes here</h1>
+const Preferences = ({ classNameIsSet, toggleClassName }) => {
+
+  const [ leftHanded, setLeftHanded ] = useState(classNameIsSet("left-handed"))
+
+  const toggleLeftHanded = () => {
+    setLeftHanded(!leftHanded)
+    toggleClassName("left-handed", !leftHanded)
+  }
+
+
+  return (
+    <div id="preferences">
+      <h1>Preferences</h1>
+      <fieldset>
+        <legend>Layout</legend>
+        <label htmlFor="left-handed">
+          <input
+            id="left-handed"
+            type="checkbox"
+            defaultChecked={leftHanded}
+            onChange={toggleLeftHanded}
+          />
+          Use left-handed layout
+        </label>
+      </fieldset>
+    </div>
  )
 }
 
