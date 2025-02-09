c00467e44ea5d1ddbc11bcade16f08848f5b1f6e
James Newton <blackslate@lexogram.com>
2025-02-09 13:34:41 +0300

    Fix backface-visibility issue on Chrome
    src/components/Activity.css
    ---
    * Tweak --origin by half a cent
    * Make .details red only when in .active card
    * Add backface-visibility: hidden; to .card.flipped
    * Use transform: none instead of rotateY(0deg) so
      cue image fits inside its borders

--- src/index.js
+++ src/index.js
@@ -3,15 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
 
-// ReactDOM.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>,
-//   document.getElementById('root')
-// );
-
 ReactDOM.render(
-  <App />,
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
   document.getElementById('root')
 );
 
